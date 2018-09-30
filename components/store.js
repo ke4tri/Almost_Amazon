549292cb9b15ea802cf5719b1c3e79a8859fc262
Wayne Collier <ke4tri@gmail.com>
2018-09-30 08:33:34 -0500

    added book to array

--- components/store.js
+++ components/store.js
@@ -7,9 +7,9 @@ const books = [
         image: "https://about.canva.com/wp-content/uploads/sites/3/2015/01/children_bookcover.png",
     },
     {
-        Title: "Samurai1",
-        Price: "$12.00",
-        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        Title: "Sex is a funny word",
+        Price: "$14.00",
+        bio: "It the truth and it has always been the truth. When it comes to words, it's the funniest",
+        image: "https://typeset-beta.imgix.net/lovelace/uploads/615/79b3cfd0-f29b-0133-2434-0e1b1c96d76b.jpg",
     }
 ]
