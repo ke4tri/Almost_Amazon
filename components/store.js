5f84e642bd4efde2e6ea65ab4680cdcae90f50eb
Wayne Collier <ke4tri@gmail.com>
2018-09-30 08:30:24 -0500

    basic html/css structure

---
+++ components/store.js
@@ -0,0 +1,15 @@
+
+const books = [
+    {
+        Title: "Fred the lonley monster",
+        Price: "$12.00",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://about.canva.com/wp-content/uploads/sites/3/2015/01/children_bookcover.png",
+    },
+    {
+        Title: "Samurai1",
+        Price: "$12.00",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+    }
+]
