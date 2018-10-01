854cede9179760cf7eac3616ae02b3d6a72a870d
Wayne Collier <ke4tri@gmail.com>
2018-09-30 19:41:27 -0500

    all the basics are working

--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -1,6 +1,25 @@
+import {addCart} from '../components/cart.js'
+import {books} from '../components/store.js'
+
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
 
-export {printToDom}
+const bookString = (bookNum) => {
+    let i = 0;
+    let newString = '';
+    for(i=0;i<books.length;i++){
+        if(bookNum === i){
+            newString += `<div id="person${[i]}" class="personAll ">`;
+            newString +=    `<img src="${books[i].image}" class="img-fluid " alt="...">`
+            newString +=    `<h7>${books[i].title}</h7>`
+            newString +=    `<h7>${books[i].price}</h7>`
+            newString +=  `</div>`
+        };
+    };
+    return newString;
+};
+
+
+export {printToDom, bookString}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,10 @@
-import {printToDom} from './helpers/util.js'
+// import {printToDom} from './helpers/util.js'
 import {addCart} from './components/cart.js'
-
 
 const cartButton1 = document.getElementById("cartButton1")
 const cartButton2 = document.getElementById("cartButton2")
 
-document.getElementById("cartButton1").addEventListener("click", addCart);
-document.getElementById("cartButton2").addEventListener("click", addCart);
+cartButton1.addEventListener("click", function (){addCart(0)}); // from here goes to cart.js
+cartButton2.addEventListener("click", function (){addCart(1)});
 
 
